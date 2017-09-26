24a71587709f9cc2dece8af003e094a2ca77bfab
Eric Windmill <ericwindmill@gmail.com>
2017-09-26 14:13:54 -0700

    Add all Grid Lesson files and update routes for all files

--- src/components/lessons/302/Routes.jsx
+++ src/components/lessons/302/Routes.jsx
@@ -4,6 +4,28 @@ import React, { Component } from 'react'
 import Intro_302 from './Intro'
 import OneGridTerms from './OneGridTerms'
 import TwoDefineAGrid  from './TwoDefineAGrid'
+import ThreeFRUnit from './ThreeFRUnit'
+import FourGridAutoColAndRow from './FourGridAuto'
+import FiveGridGap from './FiveGridGap'
+import SixRepeat from './SixRepeat'
+import SevenMinMax from './SevenMinMax'
+import EightAutoFillAndFit from './EightAutoFillAndFit'
+import NineGridTemplateAreas from './NineGridTemplateAreas'
+import TenPlacingItems from './TenPlacingItems'
+import ElevenGridRowAndColumn from './ElevenGridRowAndColumn'
+import TwelveSpan from './TwelveSpan'
+import ThirteenNamedLines from 'ThirteenNamedLines'
+import FourteenGridArea from 'FourteenGridArea'
+import SixteenGridAutoPlacement from 'SixteenGridAutoPlacement'
+import SeventeenGridAutoFlow from 'SeventeenGridAutoFlow'
+import EighteenLayering from 'EighteenLayering'
+import NineteenGridOrder from 'NineteenGridOrder'
+import TwentyJustifyItems from 'TwentyJustifyItems'
+import TwentyOneAlignItems from 'TwentyoneJustifyItems'
+import TwentyTwoJustifyAndAlignContent from 'TwentyTwoJustifyAndAlignContent'
+import TwentyThreeJustifyAndAlignSelf from 'TwentyThreeJustifyAndAlignSelft'
+
+
 
 class Routes extends Component {
   render () {
@@ -11,8 +33,27 @@ class Routes extends Component {
       <Switch>
         <Route exact path='/302/' component={Intro_302} />
         <Route exact path='/302/grid-terms' component={OneGridTerms} />
-        <Route exact path='/302/define-grid' component={TwoDefineAGrid} />
-
+        <Route exact path='/302/define-grid' component={ThreeFRUnit} />
+        <Route exact path='/302/fr-unit' component={ThreeFRUnit} />
+        <Route exact path='/302/grid-auto-col-and-row' component={FourGridAutoColAndRow} />
+        <Route exact path='/302/grid-gap' component={FiveGridGap} />
+        <Route exact path='/302/repeat' component={SixRepeat} />
+        <Route exact path='/302/minmax' component={SevenMinMax} />
+        <Route exact path='/302/autofill-autofit' component={EightAutoFillAndFit} />
+        <Route exact path='/302/grid-template-areas' component={NineGridTemplateAreas} />
+        <Route exact path='/302/placing-grid-items' component={TenPlacingItems} />
+        <Route exact path='/302/grid-row-and-column' component={ElevenGridRowAndColumn} />
+        <Route exact path='/302/span' component={TwelveSpan} />
+        <Route exact path='/302/named-lines' component={ThirteenNamedLines} />
+        <Route exact path='/302/grid-area' component={FourteenGridArea} />
+        <Route exact path='/302/grid-auto-placement' component={SixteenGridAutoPlacement} />
+        <Route exact path='/302/grid-auto-flow' component={SeventeenGridAutoFlow} />
+        <Route exact path='/302/layering' component={EighteenLayering} />
+        <Route exact path='/302/grid-order' component={NineteenGridOrder} />
+        <Route exact path='/302/justify-items' component={TwentyJustifyItems} />
+        <Route exact path='/302/align-items' component={TwentyOneAlignItems} />
+        <Route exact path='/302/justify-align-content' component={TwentyTwoJustifyAndAlignContent} />
+        <Route exact path='/302/justify-align-self' component={TwentyThreeJustifyAndAlignSelf} />
       </Switch>
     )
   }
--- src/components/lessons/302/TwoDefineAGrid.jsx
+++ src/components/lessons/302/TwoDefineAGrid.jsx
@@ -9,14 +9,51 @@ class TwoDefineGrid extends Component {
     return(
       <div>
         <h2>Defining a Grid with Display and Grid Template</h2>
+        <p>
+          Defining a grid is similar to defining a flexbox. <code>display: grid</code> defines the grid container. Unlike flexbox, we have to do a bit more to make it act like a grid.
+        </p>
+        <p>
+          The two other properties that all grids should have are <code>grid-template-columns</code> and <code>grid-template-rows</code>. These properties allow us to set the number and size of your columns and rows.
+        </p>
+        <SyntaxHighlighter
+          language='css'
+          style={monokai}
+          children={
+`.grid-container {
+  display: grid | inline-grid;
+  grid-template-columns: 100px 50px 100px;
+  grid-template-rows: 100px 100px;
+}`} />
+          <p>
+            In this example, I defined 3 columns that are 100px each. The values represent a column track width. The space inbetween (and around) each value represents a grid line. (And the same goes for rows). With this grid definition, you'll end up with a grid that has 4 vertical lines and 3 horizontal lines.
+          </p>
 
-
-
+          <img src='http://res.cloudinary.com/ericwindmill/image/upload/c_scale,w_500/v1506284099/css-layout/Define_a_Grid.png' />
+          <p>
+            <code>grid-template-columns</code> and <code>grid-template-rows</code> take any units that any size property in CSS could take.
+          </p>
+          <dl>
+            <dt>Absolute: </dt>
+            <dd><code>px</code>, <code>in</code>, <code>pt</code>, etc</dd>
+            <dt>Relative:</dt>
+            <dd><code>percentage</code></dd>
+            <dd><code>vh</code> / <code>vw</code></dd>
+            <dd>etc...</dd>
+          </dl>
+          <p>
+            In this Codepen below, you can input whichever values you want for <code>grid-template-columns</code> and <code>grid-template-rows</code>.
+          </p>
+          <Codepen
+            hash='QqGrpb'
+            user='ericwindmill'
+            tab='result'
+            height='500'
+          />
         <NextLessonNav
-            prev_path='101'
-            prev_name='Dummy'
-            next_name='Dummy'
-            next_path='101'
+            prev_path='/301/grid-terms'
+            prev_name='Basic Grid Terminology'
+            next_name='Using the fr unit'
+            next_path='/301/fr-unit'
           />
       </div>
     )
