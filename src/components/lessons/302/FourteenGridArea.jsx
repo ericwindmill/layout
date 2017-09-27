4ac443b04f0e27f7a67fa36f14ca281ab4b5ab52
Eric Windmill <ericwindmill@gmail.com>
2017-09-27 10:35:20 -0700

    Finish 13 and 14

--- src/components/lessons/302/FourteenGridArea.jsx
+++ src/components/lessons/302/FourteenGridArea.jsx
@@ -8,15 +8,61 @@ class LessonTitle extends Component {
   render () {
     return(
       <div>
-        <h2> Title </h2>
+        <h2> Grid Placement Shorthand with Grid-Area </h2>
+        <p>
+          We've briefly seen <code>grid-area</code> once before when naming grid-areas. The <code>grid-area</code> property is super flexible though. It's the only grid-item placment property you <em>need</em> to know. It'll save you time and give you the most flexibilty in your code.
+        </p>
+        <p>
+          You can pass it four grid line numbers to use it as a shorthand for the basic <code>grid-column-start</code>, <code>grid-row-start</code>, and respective 'end' properties. It also accepts named grid lines, the span keyword, and anything else that these properties accept.
+        </p>
+        <p>
+          The four values it takes, in oreder, are:
+          <ul>
+            <li><code>grid-row-start</code></li>
+            <li><code>grid-column-start</code></li>
+            <li><code>grid-row-end</code></li>
+            <li><code>grid-column-end</code></li>
+          </ul>
+          I like to remember that it takes the four properties in clockwise order, starting at the left (slighly unconventional for similar CSS properties, which start at the top).
+        </p>
 
+        <p>
+          Knowing this, you have the freedom to establish only certain properties. If you leave out the last property, for example, then <code>grid-column-end</code> is implied.
+        </p>
+
+        <SyntaxHighlighter
+          language='css'
+          style={monokai}
+          children={
+`.grid-item {
+  /* Pass it a named area */
+  grid-area: header;
+
+  /* Pass it grid-lines: (place in top-left corner) */
+  /* <grid-row-start> <grid-column-start> <grid-row-end> <grid-column-end> */
+  grid-area: 1 / 1 / 2 / 2;
+
+  /* Mix named-areas with */
+  /* This places the item in rows 1 - 3, in a column named 'main-col' */
+  grid-area: 1 / main-col / 3;
+
+  /* Using span() in grid-area */
+  /* This places the item between row lines 1 - 4 and column lines 1 and 2*/
+  grid-area: 1 / 1 / span 3 / 2;
+
+  /* With named Grid-lines*/
+  grid-area: 1 / first-col / 2 / third-col;
+}` }/>
+          <p>
+            As you can see, there are a ton of options. This is one of the tricks that makes grid highly-cusomizable with relative ease.
+          </p>
 
 
         <NextLessonNav
-            prev_path='101'
-            prev_name='Dummy'
-            next_name='Dummy'
-            next_path='101'
+            prev_path='/302/named-lines'
+            prev_name='Naming your Grid Lines'
+            next_name='Aside: Understanding The Grid Placement Algorithm'
+            next_path='/302/grid-auto-placement'
           />
       </div>
     )
--- src/components/lessons/302/ThirteenNamedLines.jsx
+++ src/components/lessons/302/ThirteenNamedLines.jsx
@@ -8,15 +8,49 @@ class LessonTitle extends Component {
   render () {
     return(
       <div>
-        <h2> Title </h2>
-
-
-
+        <h2> Named Grid Lines </h2>
+        <p>
+          Grid gives you the ability to name lines rather than choosing them by number. You name lines by placing their name inside brackets when establishing <code>grid-template-rows</code> and <code>grid-template-rows</code>. In between every track that you establish, you place the desired line-name.
+        </p>
+        <SyntaxHighlighter
+          language='css'
+          style={monokai}
+          children={
+`.grid-container {
+  display: grid;
+  grid-template-columns: [line-1] 1fr [line-2] 1fr [line-3];
+}`        }/>
+        <p>
+          You can also use named lines in <code>repeat()</code>.
+        </p>
+        <SyntaxHighlighter
+          language='css'
+          style={monokai}
+          children={
+`.grid-container {
+  display:grid;
+  grid-template-columns: repeat(2, [little-tracks] 100px) repeat(2, [lg-tracks] 200px) [grid-end];
+  /* same as: [small-tracks] 100px [small-tracks] 100px [lg-tracks] 200px [lg-tracks] 200px  [grid-end];
+}` }/>
+        <p>
+          If you name lines like that, you can then access them by giving the tracks a number:
+        </p>
+        <SyntaxHighlighter
+          language='css'
+          style={monokai}
+          children={
+`.grid-item {
+  display: grid;
+  grid-column: small-tracks 2 / grid-end;
+}` }/>
+        <p>
+          <code>small-tracks</code> without a number trailing it would use the 'first' declaration, in this case the line to the far left.
+        </p>
         <NextLessonNav
-            prev_path='101'
-            prev_name='Dummy'
-            next_name='Dummy'
-            next_path='101'
+            prev_path='/302/span'
+            prev_name='The Span Keyword'
+            next_name='Grid Placement Ultimate Shorthand: Grid-Area'
+            next_path='/302/grid-area'
           />
       </div>
     )
