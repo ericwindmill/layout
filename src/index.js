9975c9147ac4bd89887606084acef59cf9ef41b9
Eric Windmill <ericwindmill@gmail.com>
2017-09-27 13:20:01 -0700

    Implelemt ScrollToTop

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import ScrollToTop from './components/ScrollToTop'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root')
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   , root)
   registerServiceWorker()
